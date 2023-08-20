ea1f0c12e3b95c21852a28a4dd73d179a300a606
Nikita Kumawat <35212192+Nikitakumawat@users.noreply.github.com>
2023-08-20 15:10:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,105 +35,6 @@ tl.fromTo(
   { opacity: 1, y: 0, duration: 1, ease: "power2.out" }
 );
 
-function R(min, max) {
-  return min + Math.random() * (max - min);
-}
-
-function confetti() {
-  var total = 100; // How many divs to be created
-  var maxAnimationTime = 30; //Stop animation after x seconds
-  var container = document.getElementById("confettiWrapper"),
-    w = window.innerWidth,
-    h = 400;
-
-  // w = window.innerWidth , h = window.innerHeight;
-
-  TweenMax.set("#confettiWrapper", {
-    perspective: 600,
-  });
-  TweenMax.set("#confettiWrapper", {
-    xPercent: "-50%",
-    yPercent: "-50%",
-  });
-
-  for (i = 0; i < total; i++) {
-    //Create divs and assign color classes
-    var Div = document.createElement("div");
-    TweenLite.set(Div, {
-      attr: {
-        class: "confetti",
-      },
-      x: R(-80, w),
-      y: R(-200, -150),
-      z: R(-200, 200),
-    });
-    container.appendChild(Div);
-    // Add classes based on index ranges
-    if (i >= 0 && i < 10) {
-      Div.classList.add("red");
-    } else if (i >= 10 && i < 20) {
-      Div.classList.add("blue");
-    } else if (i >= 30 && i < 40) {
-      Div.classList.add("pink");
-    } else if (i >= 50 && i < 60) {
-      Div.classList.add("yellow");
-    } else if (i >= 70 && i < 80) {
-      Div.classList.add("green");
-    }
-    animm(Div);
-  }
-
-  function animm(elm) {
-    var tlConfetti = new TimelineMax({ repeat: -1 });
-    tlConfetti.timeScale(1.5); // Adjust speed. Higher number is faster
-    tlConfetti
-      .to(
-        elm,
-        R(6, 15),
-        {
-          y: h + 100,
-          opacity: R(0.2, 0.9),
-          scale: R(2, 0.25),
-          ease: Linear.easeNone,
-          repeat: -1,
-        },
-        "-=2"
-      )
-      .to(
-        elm,
-        R(4, 8),
-        {
-          x: "+=100",
-          rotationZ: R(0, 180),
-          yoyo: true,
-          ease: Sine.easeInOut,
-        },
-        "-=10"
-      )
-      .to(
-        elm,
-        R(2, 8),
-        {
-          rotationX: R(0, 360),
-          rotationY: R(0, 360),
-          yoyo: true,
-          ease: Sine.easeInOut,
-          repeat: -1,
-        },
-        "-=8"
-      );
-
-    TweenMax.delayedCall(maxAnimationTime, stopAnimation);
-
-    function stopAnimation() {
-      tlConfetti.pause();
-    }
-  }
-}
-function R(min, max) {
-  return min + Math.random() * (max - min);
-}
-
 document.addEventListener("DOMContentLoaded", function () {
   const nameCard = document.getElementById("name-card");
   const startButton = document.getElementById("start-button");
@@ -151,7 +52,6 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("choosed_bag_container").style.display = "none";
     document.getElementById("random_bag_container").style.display = "none";
     document.getElementById("question").style.display = "none";
-    document.getElementById("confettiWrapper").style.display ="none";
 
     mainScreen.style.display = "none";
     nameCard.style.display = "block";
@@ -293,16 +193,12 @@ function handleSwitchChoice(choice) {
     document.getElementById("label").innerHTML = `Balance : R ${value}`;
     musicPlayer.play();
     showWinnerModal();
-    document.getElementById("confettiWrapper").style.display ="block";
-    confetti();
   } else {
     value = userBags[0].value;
     document.getElementById("label").display = "block";
     document.getElementById("label").innerHTML = `Your bag : R ${value}`;
     musicPlayer.play();
     showWinnerModal();
-    document.getElementById("confettiWrapper").style.display ="block";
-    confetti();
   }
 }
 
@@ -342,5 +238,4 @@ function restartGame() {
   document.getElementById("question").style.display = "none";
   document.getElementById("name-card").style.display = "none";
   document.getElementById("switch_containers").style.display = "none";
-  document.getElementById("confettiWrapper").style.display ="none";
 }
